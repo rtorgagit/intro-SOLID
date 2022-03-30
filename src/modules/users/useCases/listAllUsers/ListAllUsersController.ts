682ab727221a148e0f06e4e156d4fa6803b29677
Roberto Torga <rtorga@gmail.com>
2022-03-29 23:29:10 -0300

    Implemented UseCases

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,10 +3,21 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    try {
+      const allUsers = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
+      return response.status(200).json(allUsers);
+    } catch (error) {
+      return response
+        .status(400)
+        .json({ error: "User must to be admin to list all users!" });
+    }
   }
 }
 
